42430d5716f39bc942c90484791db5bb519bf1df
Marcia Alvarez <alvarezmarciabelen@gmail.com>
2023-09-27 02:44:29 -0300

    Get a list of 20 random meals endpoint

--- controllers/meals.js
+++ controllers/meals.js
@@ -1,38 +1,46 @@
 const axios = require('axios');
 const { request, response} = require('express');
 
-const url = 'https://www.themealdb.com/api/json/v1/1/random.php'
+const url = 'https://www.themealdb.com/api/json/v1'
 
-const getRandomMeals = (req = request, res = response) => {
-    const api = process.env.API_KEY;
+const getRandomMeal = async () => {
+    const api = process.env.API_KEY;      
 
-    axios.get(`${url}`)
-        .then(({ status, data, statusText }) => {
-            // handle success
-            console.log({ status, data, statusText });
-            const {idMeal, strMeal, strArea, strMealThumb, strYoutube } = data;
-            res.status(200).json({
-                idMeal,
-                strMeal,
-                strArea,
-                strMealThumb, 
-                strYoutube
+    return new Promise((resolve, reject) => {
+        axios.get(`${url}/${api}/random.php`)
+            .then(({ status, data, statusText }) => {
+                const { meals } = data;
+                const { idMeal, strMeal, strArea, strMealThumb, strYoutube } = meals[0];
+                resolve({ idMeal, strMeal, strArea, strMealThumb, strYoutube });
+            })
+            .catch((error) => {
+                console.log(error);
+                reject(error);
             });
-        })
-        .catch((error)=>{
-            // handle error
-            console.log(error);
-            res.status(400).json({
-                status:400,
-                msg: 'Error inesperado'
-            });
-        });        
-
-
-
+    });
 }
 
+const createRandomMeals = async () => {
+    const mealList = [];
 
+    for (var i=0; i<20; i++) {
+        const data = await getRandomMeal();
+        mealList.push(data);
+        (console.log(mealList)); 
+    }
+
+    return mealList;
+}
+
+const getRandomMeals = async (req = request, res = response) => {
+    try {
+        const randomMeals = await createRandomMeals();
+        res.status(200).json(randomMeals);
+    } catch (error) {
+        console.log(error);
+        res.status(500).json({ error: 'Internal Server Error' });
+    }
+}
 
 module.exports = {
     getRandomMeals
