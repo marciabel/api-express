4b1d16e3c03cfcaf7417d62c0c7aea3241687055
Pom94 <pamelavictoria-11@hotmail.com>
2023-09-27 16:48:45 -0300

    getFilterDrink estructure

--- controllers/drinks.js
+++ controllers/drinks.js
@@ -3,33 +3,6 @@ const { request, response} = require('express');
 
 const url = 'https://www.thecocktaildb.com'
 
-const getRandomDrink = (req = request, res = response) => {
-    const api = process.env.API_KEY;
-
-    axios.get(`${url}/api/json/v1/${api}/random.php`)
-        .then(({ status, data, statusText }) => {
-            console.log({ status, data, statusText });
-            const {drinks} = data;
-            const {idDrink, strDrink, strInstructions, strDrinkThumb} = drinks[0];
-            console.log(idDrink + strDrink + strInstructions + strDrinkThumb)
-            res.status(200).json({
-                status,
-                idDrink,
-                strDrink,
-                strInstructions,
-                strDrinkThumb,
-                statusText                
-            });
-        })
-        .catch((error)=>{
-            console.log(error);
-            res.status(400).json({
-                status:400,
-                msg: 'Error inesperado'
-            });
-        });
-};
-
 
 const getRandomDrinks = (req = request, res = response) => {
     const api = process.env.API_KEY;
@@ -45,12 +18,10 @@ const getRandomDrinks = (req = request, res = response) => {
                     const { idDrink, strDrink, strInstructions, strDrinkThumb } = drinks[0];
 
                     return {
-                        status,
                         idDrink,
                         strDrink,
                         strInstructions,
-                        strDrinkThumb,
-                        statusText
+                        strDrinkThumb
                     };
                 })
                 .catch((error) => {
@@ -65,7 +36,12 @@ const getRandomDrinks = (req = request, res = response) => {
 
     Promise.all(drinksPromises)
         .then((results) => {
-            res.status(200).json(results);
+            const response = {
+                status: 200,
+                results,
+                statusText: 'OK'
+            }
+            res.status(200).json(response);
         })
         .catch((error) => {
             console.log(error);
@@ -75,6 +51,36 @@ const getRandomDrinks = (req = request, res = response) => {
             });
         });
 };
+
+const getFilterDrinks = (req = request, res = response) => {
+    const api = process.env.API_KEY;
+
+    const { f } = req.query;
+    const { c } = req.query;
+    let query_params = '';
+    console.log(f, c);
+
+    ( f != undefined) ? query_params += f && `search.php?f=${f}`:
+    ( c != undefined) ? query_params += c && `filter.php?c=${c}`:
+
+    axios.get(`${url}/api/json/v1/${api}/{query_params}`)
+        .then(({ status, data, statusText }) => {
+            console.log({ status, data, statusText });
+            const {drinks} = data;
+            res.status(200).json({
+                status,
+                drinks,
+                statusText                
+            });
+        })
+        .catch((error)=>{
+            console.log(error);
+            res.status(400).json({
+                status:400,
+                msg: 'Error inesperado'
+            });
+        });
+}
 
 const getIdDrink = (req = request, res = response) => {
     const api = process.env.API_KEY;
@@ -108,5 +114,6 @@ const getIdDrink = (req = request, res = response) => {
 
 module.exports = {
     getRandomDrinks,
+    getFilterDrinks,
     getIdDrink,
 };
--- routes/drinks.js
+++ routes/drinks.js
@@ -1,10 +1,12 @@
 const { Router } = require('express');
-const { getRandomDrinks, getIdDrink} = require('../controllers/drinks');
+const { getRandomDrinks, getFilterDrinks, getIdDrink} = require('../controllers/drinks');
 
 const routes = Router();
 
 
 routes.get('/randomDrinks', getRandomDrinks);
+routes.get('/filter_drinks', getFilterDrinks);
 routes.get('/:idDrink', getIdDrink);
+
 
 module.exports = routes;
