ff33ad33a2b7145b6fdaf764fa1278a299a19923
Marcia Alvarez <alvarezmarciabelen@gmail.com>
2023-09-27 00:05:26 -0300

    fixes in script and middleware

--- models/server.js
+++ models/server.js
@@ -1,4 +1,5 @@
 const express = require('express')
+const cors = require('cors')
 
 class Server{
 
@@ -13,17 +14,24 @@ class Server{
     }
 
     middleware() {
+        this.app.use(cors());
+
         this.app.use(express.static('public'));
     }
 
     routers() {
-        this.app.use('/api-express/v1', require('../routes/mar'))
-        this.app.use('/api-express/v1', require('../routes/pame'))
+        this.app.use('/api-express/v1/meals', require('../routes/meals'));
+        //this.app.use('/api-express/v1/drinks', require('../routes/pame'));
+    
+        this.app.all('*', (req, res) => {
+            res.status(404).json({message:'404 Page Not Found'})
+        })
+
     }
 
     listen () {
         this.app.listen (this.port, () => {
-            console.log(`App escuchando en el puerto ${process.env.PORT}`);
+            console.log(`App escuchando en el puerto ${this.port}`);
         });
     }
 }
